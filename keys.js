6866301d86e68b5b3ecaff0093b71869c9689422
Curtis Ballard <cvrvmxlcvrt28@gmail.com>
2017-07-13 21:46:16 -0700

    Update and rename animalbutton.js to keys.js

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'BV3Uvfh8RN5gU7eFtJ8WeCPDb',
+  consumer_secret: 's5OKlMTRWt3HcnPxCHMW3u0guWR2tdJ8qMVewN6fB2AEG6IOOs',
+  access_token_key: '884545130752991234-r351TYZstgMeSAMABjUKukA4HEWjaSc',
+  access_token_secret: 'sIjIefAtkyTxeeZ3pHXjUUvUmptZAd0MEIPtt7kir7BMu',
+}
+
+exports.spotifyKeys = {
+	id: '4c2c9fe751034daead2787c188c8e320',
+	secret: '0fa73ee59f54417ebad6be33905d7e68'
+}
